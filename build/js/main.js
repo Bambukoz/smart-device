25283542717561fe8ba9bc42c485354f5d2572e2
Ezhov Alexey <ezhov-a@yandex.ru>
2021-01-28 21:48:26 +0300

    Добавляет шапку и футер

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,22 @@
 'use strict';
-
-(function () {
-  var header = document.querySelector('.js-header');
-  var btnMenuOpen = document.querySelector('.js-menu-open');
-
-  var pageClass = {
-    OPEN: 'header--open',
-    NOJS: 'header--nojs',
-  };
-  header.classList.remove(pageClass.NOJS);
-
-  var menuHandler = function (evt) {
-    if (evt.target === btnMenuOpen) {
-      header.classList.add(pageClass.OPEN);
-    } else {
-      header.classList.remove(pageClass.OPEN);
-    }
-  };
-
-  document.addEventListener('click', menuHandler);
-})();
+//
+// (function () {
+//   var header = document.querySelector('.js-header');
+//   var btnMenuOpen = document.querySelector('.js-menu-open');
+//
+//   var pageClass = {
+//     OPEN: 'header--open',
+//     NOJS: 'header--nojs',
+//   };
+//   header.classList.remove(pageClass.NOJS);
+//
+//   var menuHandler = function (evt) {
+//     if (evt.target === btnMenuOpen) {
+//       header.classList.add(pageClass.OPEN);
+//     } else {
+//       header.classList.remove(pageClass.OPEN);
+//     }
+//   };
+//
+//   document.addEventListener('click', menuHandler);
+// })();
