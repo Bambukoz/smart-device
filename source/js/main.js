6ac0cdd5427569c0dbe2c4a218d4d988e287d705
Ezhov Alexey <ezhov-a@yandex.ru>
2021-02-02 21:35:03 +0300

    Вносит правки после проверки наставником

--- source/js/main.js
+++ source/js/main.js
@@ -2,91 +2,91 @@
 
 (() => {
   const KeyButton = {
-    ESCAPE: `Escape`
+    ESCAPE: 'Escape'
   };
 
   const modalClass = {
-    MODAL_FORM: `js-form-modal`,
-    MODAL_OPEN: `js-modal-open`,
-    MODAL_CLOSE: `js-modal-close`,
-    MODAL_CLASS_ACTIVE: `feedback--active`,
-    OVERLAY_ACTIVE: `overlay--active`,
-    SUCCESS_ACTIVE: `feedback__success--active`,
-    PAGE_LOCKED: `page--locked`
+    MODAL_FORM: 'js-form-modal',
+    MODAL_OPEN: 'js-modal-open',
+    MODAL_CLOSE: 'js-modal-close',
+    MODAL_CLASS_ACTIVE: 'feedback--active',
+    OVERLAY_ACTIVE: 'overlay--active',
+    SUCCESS_ACTIVE: 'feedback__success--active',
+    PAGE_LOCKED: 'page--locked'
   };
 
-  const page = document.querySelector(`.js-page`);
-  const form = document.querySelector(`.js-form`);
-  const modal = document.querySelector(`.js-modal`);
-  const modalForm = document.querySelector(`.js-form-modal`);
-  const overlay = document.querySelector(`.js-overlay`);
-  const successModal = document.querySelector(`.feedback__success`);
+  const page = document.querySelector('.js-page');
+  const form = document.querySelector('.js-form');
+  const modal = document.querySelector('.js-modal');
+  const modalForm = document.querySelector('.js-form-modal');
+  const overlay = document.querySelector('.js-overlay');
+  const successModal = document.querySelector('.feedback__success');
 
   let isStorageSupport = true;
-  let storageName = ``;
-  let storageTel = ``;
+  let storageName = '';
+  let storageTel = '';
 
   try {
-    storageName = localStorage.getItem(`userName`);
+    storageName = localStorage.getItem('userName');
   } catch (err) {
     isStorageSupport = false;
   }
 
   try {
-    storageTel = localStorage.getItem(`userTel`);
+    storageTel = localStorage.getItem('userTel');
   } catch (err) {
     isStorageSupport = false;
   }
 
-  const smoothLinks = document.querySelectorAll(`a[href^="#"]`);
-  for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener(`click`, (evt) => {
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (const smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', (evt) => {
       evt.preventDefault();
-      let id = smoothLink.getAttribute(`href`);
+      const id = smoothLink.getAttribute('href');
       if (id.length > 1) {
         document.querySelector(id).scrollIntoView({
-          behavior: `smooth`,
-          block: `start`
+          behavior: 'smooth',
+          block: 'start'
         });
       }
     });
   }
 
-  const closeModal = function () {
+  const closeModal = () => {
     modal.classList.remove(modalClass.MODAL_CLASS_ACTIVE);
     overlay.classList.remove(modalClass.OVERLAY_ACTIVE);
     successModal.classList.remove(modalClass.SUCCESS_ACTIVE);
     page.classList.remove(modalClass.PAGE_LOCKED);
-    document.removeEventListener(`keydown`, onEscModalClose);
-    document.addEventListener(`click`, openModalOnClick);
+    document.removeEventListener('keydown', onEscModalClose);
+    document.addEventListener('click', openModalOnClick);
   };
 
-  const openModal = function () {
+  const openModal = () => {
     modal.classList.add(modalClass.MODAL_CLASS_ACTIVE);
     overlay.classList.add(modalClass.OVERLAY_ACTIVE);
     successModal.classList.add(modalClass.SUCCESS_ACTIVE);
     page.classList.add(modalClass.PAGE_LOCKED);
-    document.addEventListener(`keydown`, onEscModalClose);
-    document.addEventListener(`click`, closeModalOnClick);
+    document.addEventListener('keydown', onEscModalClose);
+    document.addEventListener('click', closeModalOnClick);
   };
 
-  const onEscModalClose = function (evt) {
+  const onEscModalClose = (evt) => {
     if (evt.key === KeyButton.ESCAPE) {
       evt.preventDefault();
       closeModal();
     }
   };
 
-  const closeModalOnClick = function (evt) {
+  const closeModalOnClick = (evt) => {
     if (evt.target.classList.contains(modalClass.MODAL_CLOSE) ||
         evt.target.classList.contains(modalClass.OVERLAY_ACTIVE)) {
       closeModal();
-      document.removeEventListener(`click`, closeModalOnClick);
-      document.addEventListener(`submit`, onFormSubmit);
+      document.removeEventListener('click', closeModalOnClick);
+      document.addEventListener('submit', onFormSubmit);
     }
   };
 
-  const openModalOnClick = function (evt) {
+  const openModalOnClick = (evt) => {
     if (evt.target.classList.contains(modalClass.MODAL_OPEN)) {
       evt.preventDefault();
       modal.classList.add(modalClass.MODAL_CLASS_ACTIVE);
@@ -101,21 +101,21 @@
       } else {
         modalForm.modalName.focus();
       }
-      document.addEventListener(`keydown`, onEscModalClose);
-      document.addEventListener(`click`, closeModalOnClick);
-      document.removeEventListener(`click`, openModalOnClick);
+      document.addEventListener('keydown', onEscModalClose);
+      document.addEventListener('click', closeModalOnClick);
+      document.removeEventListener('click', openModalOnClick);
     }
   };
 
-  const onFormSubmit = function (evt) {
+  const onFormSubmit = (evt) => {
     evt.preventDefault();
     openModal();
     if (evt.target.classList.contains(modalClass.MODAL_FORM) && isStorageSupport) {
-      localStorage.setItem(`userName`, evt.target.modalName.value);
-      localStorage.setItem(`userTel`, evt.target.modalTel.value);
+      localStorage.setItem('userName', evt.target.modalName.value);
+      localStorage.setItem('userTel', evt.target.modalTel.value);
     } else {
-      localStorage.setItem(`userName`, form.name.value);
-      localStorage.setItem(`userTel`, form.tel.value);
+      localStorage.setItem('userName', form.name.value);
+      localStorage.setItem('userTel', form.tel.value);
     }
     evt.target.reset();
   };
@@ -127,6 +127,6 @@
     form.tel.value = storageTel;
   }
 
-  document.addEventListener(`submit`, onFormSubmit);
-  document.addEventListener(`click`, openModalOnClick);
+  document.addEventListener('submit', onFormSubmit);
+  document.addEventListener('click', openModalOnClick);
 })();
