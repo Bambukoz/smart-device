4f0a9a76b8821dd1ebba6d53fce3eef618dccb3e
Ezhov Alexey <ezhov-a@yandex.ru>
2021-02-11 03:59:40 +0300

    Правки после проверки

--- build/js/main.js
+++ build/js/main.js
@@ -5,14 +5,17 @@
     ESCAPE: 'Escape'
   };
 
-  const modalClass = {
+  const pageClass = {
     MODAL_FORM: 'js-form-modal',
     MODAL_OPEN: 'js-modal-open',
     MODAL_CLOSE: 'js-modal-close',
     MODAL_CLASS_ACTIVE: 'feedback--active',
     OVERLAY_ACTIVE: 'overlay--active',
     SUCCESS_ACTIVE: 'feedback__success--active',
-    PAGE_LOCKED: 'page--locked'
+    PAGE_LOCKED: 'page--locked',
+    ACCORDION_ACTIVE: 'btn__accordion--active',
+    NOJS_CLASS: 'js-nojs',
+    ACTIVE_CLASS: 'js-active'
   };
 
   const page = document.querySelector('.js-page');
@@ -21,6 +24,28 @@
   const modalForm = document.querySelector('.js-form-modal');
   const overlay = document.querySelector('.js-overlay');
   const successModal = document.querySelector('.feedback__success');
+  const accordionBtns = document.querySelectorAll('.btn__accordion');
+  const nav = document.querySelector('.js-nav');
+  const btnNav = document.querySelector('.js-nav-btn');
+  const location = document.querySelector('.js-location');
+  const btnLocation = document.querySelector('.js-location-btn');
+
+  const toggleAccordion = (evt) => {
+    if (evt.target === btnNav) {
+      btnNav.classList.toggle(pageClass.ACCORDION_ACTIVE);
+      btnLocation.classList.remove(pageClass.ACCORDION_ACTIVE);
+    } else {
+      btnLocation.classList.toggle(pageClass.ACCORDION_ACTIVE);
+      btnNav.classList.remove(pageClass.ACCORDION_ACTIVE);
+    }
+  }
+
+  for (let btn of accordionBtns) {
+    btn.classList.remove(pageClass.NOJS_CLASS);
+    btn.addEventListener('click', toggleAccordion);
+  }
+  nav.classList.remove(pageClass.NOJS_CLASS);
+  location.classList.remove(pageClass.NOJS_CLASS);
 
   let isStorageSupport = true;
   let storageName = '';
@@ -53,19 +78,19 @@
   }
 
   const closeModal = () => {
-    modal.classList.remove(modalClass.MODAL_CLASS_ACTIVE);
-    overlay.classList.remove(modalClass.OVERLAY_ACTIVE);
-    successModal.classList.remove(modalClass.SUCCESS_ACTIVE);
-    page.classList.remove(modalClass.PAGE_LOCKED);
+    modal.classList.remove(pageClass.MODAL_CLASS_ACTIVE);
+    overlay.classList.remove(pageClass.OVERLAY_ACTIVE);
+    successModal.classList.remove(pageClass.SUCCESS_ACTIVE);
+    page.classList.remove(pageClass.PAGE_LOCKED);
     document.removeEventListener('keydown', onEscModalClose);
     document.addEventListener('click', openModalOnClick);
   };
 
   const openModal = () => {
-    modal.classList.add(modalClass.MODAL_CLASS_ACTIVE);
-    overlay.classList.add(modalClass.OVERLAY_ACTIVE);
-    successModal.classList.add(modalClass.SUCCESS_ACTIVE);
-    page.classList.add(modalClass.PAGE_LOCKED);
+    modal.classList.add(pageClass.MODAL_CLASS_ACTIVE);
+    overlay.classList.add(pageClass.OVERLAY_ACTIVE);
+    successModal.classList.add(pageClass.SUCCESS_ACTIVE);
+    page.classList.add(pageClass.PAGE_LOCKED);
     document.addEventListener('keydown', onEscModalClose);
     document.addEventListener('click', closeModalOnClick);
   };
@@ -78,8 +103,8 @@
   };
 
   const closeModalOnClick = (evt) => {
-    if (evt.target.classList.contains(modalClass.MODAL_CLOSE) ||
-        evt.target.classList.contains(modalClass.OVERLAY_ACTIVE)) {
+    if (evt.target.classList.contains(pageClass.MODAL_CLOSE) ||
+            evt.target.classList.contains(pageClass.OVERLAY_ACTIVE)) {
       closeModal();
       document.removeEventListener('click', closeModalOnClick);
       document.addEventListener('submit', onFormSubmit);
@@ -87,11 +112,11 @@
   };
 
   const openModalOnClick = (evt) => {
-    if (evt.target.classList.contains(modalClass.MODAL_OPEN)) {
+    if (evt.target.classList.contains(pageClass.MODAL_OPEN)) {
       evt.preventDefault();
-      modal.classList.add(modalClass.MODAL_CLASS_ACTIVE);
-      overlay.classList.add(modalClass.OVERLAY_ACTIVE);
-      page.classList.add(modalClass.PAGE_LOCKED);
+      modal.classList.add(pageClass.MODAL_CLASS_ACTIVE);
+      overlay.classList.add(pageClass.OVERLAY_ACTIVE);
+      page.classList.add(pageClass.PAGE_LOCKED);
       if (storageName && !storageTel) {
         modalForm.modalName.value = storageName;
         modalForm.modalTel.focus();
@@ -110,7 +135,7 @@
   const onFormSubmit = (evt) => {
     evt.preventDefault();
     openModal();
-    if (evt.target.classList.contains(modalClass.MODAL_FORM) && isStorageSupport) {
+    if (evt.target.classList.contains(pageClass.MODAL_FORM) && isStorageSupport) {
       localStorage.setItem('userName', evt.target.modalName.value);
       localStorage.setItem('userTel', evt.target.modalTel.value);
     } else {
--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task("refresh", function (done) {
 gulp.task("images", function() {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.optipng({optimizationLevel: 1}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
@@ -63,7 +63,7 @@ gulp.task("images", function() {
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
+    .pipe(webp({quality: 80}))
     .pipe(gulp.dest("source/img"));
 });
 
